5ce0e7651a96fe92bc3a3de9a6292c87deb8f85a
Bruno Ciccio <dev.bruno.ciccio@gmail.com>
2024-03-07 02:04:28 -0300

    Update da página do InkeVerseApplication - SpringBootTest

--- src/main/java/br/com/inkverse/inkverse/InkverseApplication.java
+++ src/main/java/br/com/inkverse/inkverse/InkverseApplication.java
@@ -2,12 +2,21 @@ package br.com.inkverse.inkverse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.junit.jupiter.api.Test;
 
 @SpringBootApplication
 public class InkverseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InkverseApplication.class, args);
+	}
+
+}
+@SpringBootTest
+class InkverseApplicationTests {
+
+	@Test
+	void contextLoads() {
 	}
 
 }
