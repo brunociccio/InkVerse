9c1f70c8e573fce3d4568573154f47e32063c7cc
Bruno Ciccio <121205642+brunociccio@users.noreply.github.com>
2024-03-14 11:22:39 -0300

    Delete src/main directory

--- src/main/java/br/com/inkverse/repository/LoginRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.inkverse.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.inkverse.model.Login;
-
-public interface LoginRepository extends JpaRepository<Login, String> {
-    
-}
