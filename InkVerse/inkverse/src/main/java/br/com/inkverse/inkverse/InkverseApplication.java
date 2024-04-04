6c994ec2e96f3331d0c966e77b4f8227d91fd422
dev bruno ciccio <dev.bruno.ciccio@gmail.com>
2024-04-04 00:38:04 -0300

    update da classe de aplcação com dois novos imports para retornar "WebApp Inkverse"

--- InkVerse/inkverse/src/main/java/br/com/inkverse/inkverse/InkverseApplication.java
+++ InkVerse/inkverse/src/main/java/br/com/inkverse/inkverse/InkverseApplication.java
@@ -2,12 +2,22 @@ package br.com.inkverse.inkverse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
 public class InkverseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InkverseApplication.class, args);
+	}
+
+	@RequestMapping
+	@ResponseBody
+	public String home(){
+		return "WebApp Inkverse";
 	}
 
 }
