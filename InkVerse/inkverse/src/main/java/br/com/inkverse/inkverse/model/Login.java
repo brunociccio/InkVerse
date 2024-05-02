ffb4c3e3a2f2801ba02faf760aec0217452251ce
dev bruno ciccio <dev.bruno.ciccio@gmail.com>
2024-05-02 01:41:35 -0300

    remoção da dependencia de IA e atualização da dependencia do lombook

--- InkVerse/inkverse/src/main/java/br/com/inkverse/inkverse/model/Login.java
+++ InkVerse/inkverse/src/main/java/br/com/inkverse/inkverse/model/Login.java
@@ -1,11 +1,11 @@
 package br.com.inkverse.inkverse.model;
 
-import groovy.transform.builder.Builder;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
