7a4db200ba76d3ddb542e3b3ac242659de02dc4e
Bruno Ciccio <dev.bruno.ciccio@gmail.com>
2024-05-07 20:23:21 -0300

    @ metodo - para status code do CRUD da classe Cadastro Controller

--- InkVerse/inkverse/src/main/java/br/com/inkverse/inkverse/controller/CadastroController.java
+++ InkVerse/inkverse/src/main/java/br/com/inkverse/inkverse/controller/CadastroController.java
@@ -7,6 +7,7 @@ import static org.springframework.http.HttpStatus.NO_CONTENT;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +16,8 @@ import org.springframework.web.server.ResponseStatusException;
 import br.com.inkverse.inkverse.model.Cadastro;
 import br.com.inkverse.inkverse.repository.CadastroRepository;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 import jakarta.validation.Valid;
@@ -43,6 +46,11 @@ public class CadastroController {
     // POST
     @PostMapping
     @ResponseStatus(CREATED)
+    @ApiResponses({
+        @ApiResponse(responseCode = "201", description = "Cadastro criado com sucesso"),
+        @ApiResponse(responseCode = "400", description = "Validação falhou. Verifique o corpo da requisição")
+    })
+    @CacheEvict(allEntries = true)
     public Cadastro create(@Valid @RequestBody Cadastro cadastro) {
         log.info("Criando cadastro: {}", cadastro);
         return repository.save(cadastro);
@@ -58,6 +66,7 @@ public class CadastroController {
 
     // PUT
     @PutMapping("{id}")
+    @CacheEvict(allEntries = true)
     public Cadastro update(@PathVariable Long id, @Valid @RequestBody Cadastro cadastro) {
         log.info("Atualizando cadastro {} para {}", id, cadastro);
         return repository.findById(id)
@@ -71,6 +80,7 @@ public class CadastroController {
     // DELETE
     @DeleteMapping("{id}")
     @ResponseStatus(NO_CONTENT)
+    @CacheEvict(allEntries = true)
     public void destroy(@PathVariable Long id) {
         log.info("Apagando cadastro: {}", id);
         repository.deleteById(id);
