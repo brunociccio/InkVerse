43225f3c1e210a1f11bfd1dba1961870bc99b3a4
Bruno Ciccio <dev.bruno.ciccio@gmail.com>
2024-05-07 19:38:07 -0300

    Remoção dos imports que estão sem utilização

--- InkVerse/inkverse/src/main/java/br/com/inkverse/inkverse/config/DataBaseSeeder.java
+++ InkVerse/inkverse/src/main/java/br/com/inkverse/inkverse/config/DataBaseSeeder.java
@@ -2,15 +2,9 @@ package br.com.inkverse.inkverse.config;
 
 import org.springframework.boot.CommandLineRunner;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
-import br.com.inkverse.inkverse.model.*;
 import br.com.inkverse.inkverse.repository.*;
 
 @Configuration
@@ -18,6 +12,9 @@ public class DataBaseSeeder implements CommandLineRunner {
 
     @Autowired
     LoginRepository loginRepository;
+
+    @Autowired
+    CadastroRepository cadastroRepository;
 
     @Override
     public void run(String... args) throws Exception {
